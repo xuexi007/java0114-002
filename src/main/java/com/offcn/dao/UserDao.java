4cf93c8a1efe914c376bac4d3445806c28abe473
xuexi007 <44258352+xuexi007@users.noreply.github.com>
2019-06-26 16:10:04 +0800

    Update UserDao.java

--- src/main/java/com/offcn/dao/UserDao.java
+++ src/main/java/com/offcn/dao/UserDao.java
@@ -4,6 +4,6 @@ import com.offcn.po.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User,Long> {
-//新增方法1
+//****************************新增方法1************************************************
 public User findByNameContaining(String keyword);
 }
