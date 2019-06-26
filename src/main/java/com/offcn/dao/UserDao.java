24ecde970ca6251394a8c373b31cd46d878bfbc1
java0114 <hk109@126.com>
2019-06-26 17:12:03 +0800

    开发者2新增方法

--- src/main/java/com/offcn/dao/UserDao.java
+++ src/main/java/com/offcn/dao/UserDao.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserDao extends JpaRepository<User,Long> {
 //****************************新增方法1************************************************
 public User findByNameContaining(String keyword);
+
+//开发者2新增方法
+
+    public void demo3(String name);
 }
